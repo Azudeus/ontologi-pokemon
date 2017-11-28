8e8de057ee452ad7d4b9def80f327c2dfadc0a51
Azudeus <blitzthunder68@yahoo.com>
2017-11-28 13:40:51 +0700

    Add getWeaknessPoint

--- pokemon/example.py
+++ pokemon/example.py
@@ -1,4 +1,7 @@
 from pokemon import Pokemon
+from type import getWeaknessPoint
 
 poke1 = Pokemon("Bulbasaur", ["GRASS", "POISON"], ["NORMAL", "GRASS", "POISON"], 80, 82, 83, 100, 80)
 print(poke1)
+
+print(getWeaknessPoint(["GRASS", "POISON"], "FIRE"))

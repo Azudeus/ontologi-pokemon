cfe7f1a7f0a2e8b7c185e8e13eeec39f4a8948f7
hanmajid <han.majid2004@gmail.com>
2017-11-28 20:00:00 +0700

    add pokemon class

---
+++ pokemon/example.py
@@ -0,0 +1,4 @@
+from pokemon import Pokemon
+
+poke1 = Pokemon("Bulbasaur", ["GRASS", "POISON"], ["NORMAL", "GRASS", "POISON"], 80, 82, 83, 100, 80)
+print(poke1)
---
+++ pokemon/pokemon.py
@@ -0,0 +1,13 @@
+class Pokemon:
+	def __init__(self, name, types, move_types, hp, attack, defense, special, speed):
+		self.name = name 				# string
+		self.types = types 				# list of string
+		self.move_types = move_types 	# list of string
+		self.hp = hp 					# int
+		self.attack = attack 			# int
+		self.defense = defense 			# int
+		self.special = special 			# int
+		self.speed = speed 				# int
+
+	def __str__(self):
+		return "\nName: %s\nTypes: %s\nMove Types: %s\nHP: %s\nATK: %s\nDEF: %s\nSPC: %s\nSPD: %s\n" % (self.name, ', '.join(self.types), ', '.join(self.move_types), self.hp, self.attack, self.defense, self.special, self.speed)
